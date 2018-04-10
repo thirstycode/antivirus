e2a7c859c4bd7028dadd825f9cd970c696165ab8
Thirsty Code <37847256+thirstycode@users.noreply.github.com>
2018-04-11 03:52:23 +0530

    Add files via upload

--- checkup.py
+++ checkup.py
@@ -1,15 +1,16 @@
 
 # <-----no use ------->
 import pandas
-file = pandas.read_csv("database.csv",low_memory=False)
-virus_md5=[]
-for i in file["md5"]:
-    virus_md5.append(i[2:-1])
+file = pandas.read_csv("database_total.csv",low_memory=False)
+virus_md5 = str(file["md5"])
+virus_name = str(file["name"])
 
-
-
-virus_signature = 'b8f76630a62c4f0fe708cdb06c5c844b'
+# print(virus_name)
+print(virus_name[10])
+virus_signature = '047395207d2e40681592be7f7b33c49e'
 if virus_signature in virus_md5:
     print("virus found")
+    print(virus_md5.index(virus_signature))
+    print(virus_name[int(virus_md5.index(virus_signature))])
 else:
     print("not found")
