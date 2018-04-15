137794294344ff97b42f223a2d8bf307b52198ae
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-15 21:00:01 +0530

    drives list test

--- drives.py
+++ drives.py
@@ -1,4 +1,9 @@
+# <----NO Use in orignal code------>
+
+
+# ---TO get list of drives in hardrive---
 import win32api
+
 
 drives = win32api.GetLogicalDriveStrings()
 drives = drives.split('\000')[:-1]
