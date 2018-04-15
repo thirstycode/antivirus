82b06e09eb1deb228cf6ce349c08d44e2314beac
Pratik Kinage <37847256+thirstycode@users.noreply.github.com>
2018-04-15 20:56:57 +0530

    of no use , just added to check things

--- checkup_name.py
+++ checkup_name.py
@@ -1,10 +1,19 @@
 
 # <-----no use ------->
+# <-----no use ------->
+# <-----no use ------->
 import pandas
 file = pandas.read_csv("database_name.csv",low_memory=False)
 virus_name = str(file["name"])
 
 # print(virus_name)
 
-# <------ye niche wali cheez print nahi hori---->  dekh to ekbar
+# <------ye niche wali cheez print nahi hori---->
 print(virus_name[10])
+
+
+
+# <------solved solved solved---->
+# <------solved solved solved---->
+# <------solved solved solved---->
+# <------solved solved solved---->
